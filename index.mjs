1a033a40ebf5d3be54c6106d6d88d683d1eb3e01
okediarthur <okediarthur@gmail.com>
2024-06-15 12:40:22 +0300

    Link Logic Implemented

--- index.mjs
+++ index.mjs
@@ -5,6 +5,9 @@ list.append(3);
 list.append(20);
 list.append(30);
 list.prepend(2);
+list.pop();
+list.insertAt(100,2);
+list.removeAt(1);
 
 
 console.log(JSON.stringify(list, null, 2));
--- linkedList.mjs
+++ linkedList.mjs
@@ -27,6 +27,137 @@ class LinkedList {
         this.head = newNode;
         this.head.nextNode = prevHead;
     }
+
+    size () {
+        if(!this.head){
+            return 0;
+        }
+        let numberOfNodes = 1;
+        let current = this.head;
+        while(current.nextNode){
+            current = current.nextNode;
+            numberOfNodes += 1;
+        }
+        return numberOfNodes;
+    }
+
+    headS() {
+        if(!this.head) return null;
+        return this.head;
+    }
+
+    tail () {
+        if(!this.head) return null;
+        let current = this.head;
+        while(current.nextNode){
+            current = current.nextNode;
+        }
+        return current;
+    }
+    at (index) {
+        if( 
+            typeof index !== 'number' ||
+            !this.head ||
+            index > this.size() - 1 ||
+            index < 0
+        ) {
+            return null;
+        }
+        let currentIndex = 0;
+        let current = this.head;
+        while(currentIndex < index) {
+            current = current.nextNode;
+            currentIndex += 1;
+        }
+        return current;
+    }
+
+    pop () {
+        if(!this.head) return
+        if(!this.head.nextNode){
+            this.head = null;
+            return
+        }
+        let prevNode = this.head;
+        let current = this.head.nextNode;
+        while(current.nextNode) {
+            prevNode = current;
+            current = current.nextNode;
+        }
+        prevNode.nextNode = null;
+    }
+
+    find (value) {
+        let current = this.head;
+        let index = 0;
+        while(current) {
+            if(current.value === value) return index;
+            current = current.nextNode;
+            index += 1
+        }
+        return null;
+    }
+
+    toString() {
+        let list = '';
+        let current = this.head;
+        while (current) {
+            list += current.value;
+            list += '->'
+            current = current.nextNode;
+        }
+        list += 'null';
+        return list; 
+    }
+    insertAt(value, index) {
+        if(
+            typeof index !== 'number' ||
+            !this.head ||
+            index> this.size() ||
+            index < 0
+        ) {
+            return;
+        }
+        const newNode = new Node(value);
+        if(index === 0){
+            newNode.nextNode = this.head;
+            this,head = newNode;
+            return;
+        }
+        let currentIndex = 0;
+        let prevNode = null;
+        let current = this.head;
+        while(currentIndex < index) {
+            prevNode = current;
+            current = current.nextNode;
+            currentIndex += 1;
+        }
+        newNode.nextNode = current;
+        prevNode.nextNode = newNode;
+    }
+    removeAt(index) {
+        if(
+            typeof index !== 'number' ||
+            !this.head ||
+            index > this.size() - 1 ||
+            index < 0
+        ){
+            return;
+        }
+        if(index === 0){
+            this.head = this.head.nextNode;
+            return;
+        }
+        let prevNode = null;
+        let current = this.head;
+        let currentIndex = 0;
+        while (currentIndex < index){
+            prevNode = current;
+            current = current.nextNode;
+            currentIndex += 1;
+        }
+        prevNode.nextNode = current.nextNode;
+    }
 }
 
 export default LinkedList;
